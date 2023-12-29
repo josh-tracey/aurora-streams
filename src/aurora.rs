4912e7edda53422ef62ca4e3d435a05489d13ace
Joshua Tracey <joshua.tracey08@gmail.com>
2023-12-29 12:11:49 +1000

    tweaks: small changes

--- src/aurora.rs
+++ src/aurora.rs
@@ -29,10 +29,7 @@ impl AuroraStreams {
         }
     }
 
-    pub async fn create_channel(
-        &'static self,
-        channel_name: String,
-    ) -> tokio::task::JoinHandle<()> {
+    pub async fn create_channel(&'static self, channel_name: String) -> Result<(), String> {
         let channel = Channel::new();
         let mut channels = self.channels.lock().await;
         channels.insert(channel_name.clone(), channel);
@@ -41,11 +38,18 @@ impl AuroraStreams {
             .redis_client
             .get_async_connection()
             .await
-            .unwrap()
+            .or(Err("Failed to connect to redis".to_string()))?
             .into_pubsub();
-        pub_sub.subscribe(channel_name.clone()).await.unwrap();
+        pub_sub
+            .subscribe(channel_name.clone())
+            .await
+            .or(Err(format!(
+                "Failed to subscribe to channel {}",
+                channel_name
+            )
+            .to_string()))?;
 
-        let handle = tokio::spawn(async move {
+        tokio::spawn(async move {
             while let Some(msg) = pub_sub.on_message().next().await {
                 let channel_name = msg.get_channel_name().to_string();
                 let message: String = msg.get_payload().unwrap();
@@ -55,26 +59,37 @@ impl AuroraStreams {
             }
         });
 
-        return handle;
+        return Ok(());
     }
 
-    pub async fn publish(&self, channel_name: String, message: String) {
+    pub async fn publish(&self, channel_name: String, message: String) -> Result<(), String> {
         let channels = self.channels.lock().await;
-        let sender = &channels.get(&channel_name).unwrap().sender;
+        let sender = &channels
+            .get(&channel_name)
+            .ok_or(format!("Channel {} does not exist", channel_name).to_string())?
+            .sender;
         sender.send(message.clone()).unwrap();
         let mut con = self.redis_client.get_async_connection().await.unwrap();
         con.publish::<&str, &str, Value>(&channel_name, &message)
             .await
-            .unwrap();
+            .or(Err(format!(
+                "Failed to publish message {} to channel {}",
+                message, channel_name
+            )))?;
+        Ok(())
     }
 
     pub async fn subscribe<'a>(
         &'a self,
         channel_name: String,
         observer: impl Fn(String) + Send + 'a + 'static,
-    ) -> tokio::task::JoinHandle<()> {
+    ) -> Result<tokio::task::JoinHandle<()>, String> {
         let channels = self.channels.lock().await;
-        let mut receiver = channels.get(&channel_name).unwrap().receiver.clone();
+        let mut receiver = channels
+            .get(&channel_name)
+            .ok_or(format!("Channel {} does not exist", channel_name).to_string())?
+            .receiver
+            .clone();
 
         let handle = tokio::spawn(async move {
             let sleep = tokio::time::sleep(tokio::time::Duration::from_millis(1000));
@@ -94,6 +109,6 @@ impl AuroraStreams {
             }
         });
 
-        return handle;
+        return Ok(handle);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::error::Error;
+
 use aurora_streams::create_stream;
 
 fn test_observer(message: String) {
@@ -5,16 +7,23 @@ fn test_observer(message: String) {
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn Error>> {
     let stream = create_stream("redis://localhost:6379");
 
-    let test_channel = stream.create_channel("test_channel".to_string()).await;
+    let _ = stream.create_channel("test_channel".to_string()).await;
+    let _ = stream.create_channel("test_channel2".to_string()).await;
 
     let task_1 = stream
         .subscribe("test_channel".to_string(), test_observer)
         .await;
 
     let task_2 = stream
+        .subscribe("test_channel2".to_string(), |message| {
+            println!("3 Message recieved: {}", message);
+        })
+        .await;
+
+    let task_3 = stream
         .subscribe("test_channel".to_string(), |message| {
             println!("2 Message recieved: {}", message);
         })
@@ -22,9 +31,15 @@ async fn main() {
 
     stream
         .publish("test_channel".to_string(), "Hello World!".to_string())
-        .await;
+        .await?;
 
-    for handle in vec![test_channel, task_1, task_2] {
-        handle.await.unwrap();
+    stream
+        .publish("test_channel2".to_string(), "What's up".to_string())
+        .await?;
+
+    for handle in vec![task_1, task_2, task_3] {
+        handle?.await?;
     }
+
+    Ok(())
 }
