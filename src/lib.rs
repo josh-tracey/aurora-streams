14d1a6db6304166b91b5b45bbea539ca16ea442a
Joshua Tracey <joshua.tracey08@gmail.com>
2023-12-30 12:22:07 +1000

    fix: small fix

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,11 @@ use aurora::AuroraStreams;
 
 pub mod aurora;
 
-pub fn create_stream(#[cfg(feature = "redis")] url: &str) -> &'static AuroraStreams {
-    #[cfg(feature = "redis")]
+pub fn create_stream(#[cfg(feature = "event-routing")] url: &str) -> &'static AuroraStreams {
+    #[cfg(feature = "event-routing")]
     let client = redis::Client::open(url).unwrap();
     let streams = Arc::new(AuroraStreams::new(
-        #[cfg(feature = "logging")]
+        #[cfg(feature = "event-routing")]
         client,
     ));
     let a_streams: &'static AuroraStreams = Box::leak(Box::new(streams.clone()));
